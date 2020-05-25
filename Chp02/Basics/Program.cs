d838ef509589988400cda19a7c4ece4d7e74e3a6
alucard <alucard@pop-os.localdomain>
2020-05-25 10:59:41 -0700

    Initial Sync for exercises

---
+++ Chp02/Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double heightInMetres = 1.88;
+
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+        }
+            
+    }
+}
